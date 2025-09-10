196b9516f2bed60dcef66e5faef55b6a053cd1a3
zalulw <polyaakzalan394@gmail.com>
2025-09-10 11:30:31 +0200

    local storage

--- kr 02/penzvalto/main.js
+++ kr 02/penzvalto/main.js
@@ -1,4 +1,12 @@
+if (localStorage.getItem("number")) {
+  document.getElementById("forint").value = localStorage.getItem("number");
+}
 document.getElementById("convert").addEventListener("click", function () {
+  alert("átvált");
+
+  const value = Number(document.getElementById("forint").value);
+  localStorage.setItem("number", value);
+
   const forint = parseFloat(document.getElementById("forint").value);
   const currency = document.getElementById("currency").value;
   let result;
