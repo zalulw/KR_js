db54a553536d6a319c028b5160679b5cc810875d
zalulw <polyakzalan394@gmail.com>
2025-11-18 08:45:12 +0100

    carousel

---
+++ Carousel/src/types/Car.ts
@@ -0,0 +1,13 @@
+export type Car = {
+  marka: string;
+  modell: string;
+  evjarat: number;
+  futas_km: number;
+  uzemanyag: string;
+  valto: string;
+  szin: string;
+  ar: number;
+  images: string[];
+  leiras: string;
+  id?: number;
+};
